34814e294ef1d551b591c47dfe155059b9304c64
d-rivers <d.rivers.wtp@gmail.com>
2016-03-14 20:01:26 +0000

    css autoprefixes

--- gulpfile.babel.js/tasks/scss.js
+++ gulpfile.babel.js/tasks/scss.js
@@ -1,6 +1,7 @@
 import gulp from 'gulp';
 import gulpSass from 'gulp-sass';
 import sourcemaps from 'gulp-sourcemaps';
+import autoprefixer from 'gulp-autoprefixer';
 
 /**
  * custom scss
@@ -8,6 +9,10 @@ import sourcemaps from 'gulp-sourcemaps';
 export function scss() {
   return gulp.src('source/scss/app.scss')
     .pipe(sourcemaps.init())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false,
+    }))
     .pipe(gulpSass.sync({
       outputStyle: 'compressed',
     }).on('error', gulpSass.logError))
@@ -21,6 +26,10 @@ export function scss() {
 export function bootstrapScss() {
   return gulp.src('bower_components/bootstrap/scss/bootstrap-flex.scss')
     .pipe(sourcemaps.init())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false,
+    }))
     .pipe(gulpSass.sync({
       outputStyle: 'compressed',
     }).on('error', gulpSass.logError))
@@ -34,6 +43,10 @@ export function bootstrapScss() {
 export function iconicScss() {
   return gulp.src('source/iconic/scss/iconic-glyphs.scss')
     .pipe(sourcemaps.init())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false,
+    }))
     .pipe(gulpSass.sync({
       outputStyle: 'compressed',
     }).on('error', gulpSass.logError))

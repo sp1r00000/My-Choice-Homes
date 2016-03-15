a6f2df066ab3298e4c8740ffee5d3ad891872be2
d-rivers <d.rivers.wtp@gmail.com>
2016-03-15 00:28:01 +0000

    cached length

--- source/javascript/helpers/for-each.js
+++ source/javascript/helpers/for-each.js
@@ -3,7 +3,7 @@
  * eg querySelectorAll
  */
 export function forEach(array, cb, scope) {
-  for (let i = 0; i < array.length; i++) {
+  for (let i = 0, l = array.length; i < l; i++) {
     cb.call(scope, i, array[i]);
   }
 }

a57594c6932277bbe35fc28801aeb3afbaa3bfe4
d-rivers <d.rivers.wtp@gmail.com>
2016-03-15 01:01:06 +0000

    navbar basics

--- source/javascript/global/navbar.js
+++ source/javascript/global/navbar.js
@@ -1,7 +1,7 @@
 import { forEach } from '../helpers/for-each';
 
 /**
- * navbar function
+ * open / close mobile menu
  */
 export function toggleNavbar() {
   const icon = document.querySelector('[data-glyph="menu"');
@@ -12,12 +12,16 @@ export function toggleNavbar() {
   });
 }
 
+/**
+ * set height of menu items
+ */
 export function navItem() {
+  const fixed = document.querySelector('.mch-fixed-navbar').clientHeight;
   const nav = document.querySelector('.mch-navbar');
   const links = document.querySelectorAll('.mch-navbar a');
-  const height = nav.clientHeight / links.length;
+  const height = (nav.clientHeight - fixed) / links.length;
 
-  forEach(links, (index) => {
+  forEach(links, index => {
     links[index].style.height = `${height}px`;
   });
 }

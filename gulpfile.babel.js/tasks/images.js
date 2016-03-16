751220ec273ae4185fd61338afb6fa50c4d70b34
d-rivers <d.rivers.wtp@gmail.com>
2016-03-16 11:57:29 +0000

    images optimised in build

--- gulpfile.babel.js/tasks/images.js
+++ gulpfile.babel.js/tasks/images.js
@@ -1,9 +1,16 @@
 import gulp from 'gulp';
+import imagemin from 'gulp-imagemin';
+import pngquant from 'imagemin-pngquant';
 
 /**
  * images
  */
 export function images() {
   return gulp.src('source/images/**/*')
+    .pipe(imagemin({
+      progressive: true,
+      svgoPlugins: [{ removeViewBox: false }],
+      use: [pngquant()],
+    }))
     .pipe(gulp.dest('public/assets/images'));
 }

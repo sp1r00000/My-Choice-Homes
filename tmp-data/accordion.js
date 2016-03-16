74234a14a6450db6cd1e339fd544d8fe235fa307
d-rivers <d.rivers.wtp@gmail.com>
2016-03-16 02:49:26 +0000

    html fixes

---
+++ tmp-data/accordion.js
@@ -0,0 +1,39 @@
+module.exports = {
+  accordion: [
+    {
+      icon: 'pencil',
+      title: 'My Choice School',
+      content: '<p>My Choice School is the education provision at My Choice' +
+      ' Children\'s Homes and we provide an individualised education' +
+      ' programme within each of our homes for young people who have' +
+      ' disengaged from learning and are unable to access larger school' +
+      ' environments due to their complex emotional and behavioural' +
+      ' difficulties.</p>',
+    },
+    {
+      icon: 'people',
+      title: 'Our Staff',
+      content: '<p>Our most valuable asset is our staff team which includes' +
+      ' nearly 110 members. Our team have a wealth of experience with their' +
+      ' child centered approach and genuine passion and commitment to look' +
+      ' after our young people.</p>',
+    },
+    {
+      icon: 'excerpt',
+      title: 'About Us',
+      content: '<p>My Choice was founded in January 1999. We are a growing' +
+      ' company providing residentialcare for \'Looked After Children\'. We' +
+      ' are a child orientated, needs led organisation; established to' +
+      ' provide a positive life enhancing opportunity for young people who' +
+      ' are unable to sustain a placement in their parental homes, foster' +
+      ' homes or conventional children\'s homes.</p>',
+    },
+    {
+      icon: 'list-rich',
+      title: 'What do people say',
+      content: '<p>It\'s important that we know how parents, professionals,' +
+      ' social workers and young people rate our service, we don\'t just' +
+      ' guess, we ask...</p>',
+    },
+  ],
+};

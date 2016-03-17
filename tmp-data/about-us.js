5c0242df479c573d28cdeb7e1c4afcbd1a65ade1
d-rivers <d.rivers.wtp@gmail.com>
2016-03-17 22:59:40 +0000

    about us breakpoints

--- tmp-data/about-us.js
+++ tmp-data/about-us.js
@@ -1,3 +1,10 @@
 module.exports = {
   bundle: 'about-us',
+  header: {
+    title: 'About us',
+    subtitle: {
+      first: 'Find out who we are,',
+      second: 'what we\'re all about, and what we have to offer',
+    },
+  },
 };

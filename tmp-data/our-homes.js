aa52a753014b459eddbb256d798104dac60bebdc
d-rivers <d.rivers.wtp@gmail.com>
2016-03-19 18:26:54 +0000

    our homes mobile init

--- tmp-data/our-homes.js
+++ tmp-data/our-homes.js
@@ -6,4 +6,46 @@ module.exports = {
       second: 'homely environments for every child',
     },
   },
+  homes: [
+    {
+      home: 'Ocean Pearl',
+      shortDescription: 'some short description here',
+      thumbnail: '/assets/images/our-homes/thumbs/ocean-pearl.jpg',
+    },
+    {
+      home: 'Kestral House',
+      shortDescription: 'some short description here',
+      thumbnail: '/assets/images/our-homes/thumbs/kestral-house.jpg',
+    },
+    {
+      home: 'Kingfisher View',
+      shortDescription: 'some short description here',
+      thumbnail: '/assets/images/our-homes/thumbs/kingfisher-view.jpg',
+    },
+    {
+      home: 'Oak House',
+      shortDescription: 'some short description here',
+      thumbnail: '/assets/images/our-homes/thumbs/oak-house.jpg',
+    },
+    {
+      home: 'Pebble House',
+      shortDescription: 'some short description here',
+      thumbnail: '/assets/images/our-homes/thumbs/pebble-house.jpg',
+    },
+    {
+      home: 'Maple House',
+      shortDescription: 'some short description here',
+      thumbnail: '/assets/images/our-homes/thumbs/maple-house.jpg',
+    },
+    {
+      home: 'Ivy Cottage',
+      shortDescription: 'some short description here',
+      thumbnail: '/assets/images/our-homes/thumbs/ivy-cottage.jpg',
+    },
+    {
+      home: 'Osprey House',
+      shortDescription: 'some short description here',
+      thumbnail: '/assets/images/our-homes/thumbs/osprey-house.jpg',
+    },
+  ],
 };

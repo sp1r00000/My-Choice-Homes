d5970101de4ca1c31c3002f18620117d9c9d99e6
d-rivers <d.rivers.wtp@gmail.com>
2016-03-20 01:27:12 +0000

    single home breakpoints

--- tmp-data/our-homes/oak-house.js
+++ tmp-data/our-homes/oak-house.js
@@ -1,13 +1,19 @@
 module.exports = {
   header: {
-    title: 'Our Homes',
+    title: 'Oak House',
     subtitle: {
       first: 'We provide',
       second: 'homely environments for every child',
     },
   },
   home: 'Oak House',
-  thumbnail: '/assets/images/our-homes/thumbs/oak-house.jpg',
+  image: '/assets/images/our-homes/oak-house.jpg',
+  images: [
+    '/assets/images/our-homes/oak-house.jpg',
+    'http://placehold.it/900x600',
+    'http://placehold.it/900x600',
+    'http://placehold.it/900x600',
+  ],
   location: 'East Sussex',
   description: 'Oak House is a large detached home, set within extensive gardens in East Sussex. All bedrooms are double sized and there is a large kitchen and dining room. There are two lounge rooms where young people can spend time together and with staff to watch television, play games or generally relax.',
   facilities: 'Oak House accommodates three young people and has three large young people’s bedrooms which are decorated and furnished to a high standard. Each bedroom is personalised to meet each child’s individual needs. There is facility for three staff members to sleep in to maintain the children’s one to one staffing ratio. The home has a fully fitted kitchen that the children are able to access at all times along with the lounge and separate dining room. These rooms are furnished to meet the needs of children in a style chosen by the children and are all situated on the ground floor with a separate laundry room. The home has a bathroom for the children on the first floor, and a separate staff bathroom on the ground floor. There is a registered My Choice School attached to the side of the home in a annex. Each home is fitted with an alarm monitoring system to help safeguard the young people, this alarm is situated on all young people’s bedrooms doors and external doors with its purpose being to alert staff through the night if a young person requires support.',

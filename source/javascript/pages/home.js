c6dd9c6e52a37ede26f8ccec35c781b147f0d4cb
d-rivers <d.rivers.wtp@gmail.com>
2016-03-27 08:49:57 +0100

    accordion fix

--- source/javascript/pages/home.js
+++ source/javascript/pages/home.js
@@ -1,3 +1,23 @@
-import accordion from '../components/accordion';
+/**
+ * toggle class for accordion
+ * content
+ */
+const accordion = function accordion() {
+  const accord = document.querySelector('.accordion');
+
+  accord.addEventListener('click', event => {
+    const target = event.target;
+    const isTitle = target.className === 'accordion-title';
+    const child = target.children.length === 0;
+
+    if (isTitle) {
+      target.nextElementSibling.classList.toggle('open');
+    }
+
+    if (child) {
+      target.parentElement.nextElementSibling.classList.toggle('open');
+    }
+  });
+};
 
 export default accordion();

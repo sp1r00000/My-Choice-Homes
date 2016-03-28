f98723daeb96156aa9effc53f0b61d1721957b3e
d-rivers <d.rivers.wtp@gmail.com>
2016-03-28 19:41:53 +0100

    ie11 fix + improved scss task

--- gulpfile.babel.js/tasks/scss.js
+++ gulpfile.babel.js/tasks/scss.js
@@ -1,20 +1,28 @@
 import gulp from 'gulp';
 import sass from 'gulp-ruby-sass';
 import sourcemaps from 'gulp-sourcemaps';
-import prefix from 'gulp-autoprefixer';
+import postcss from 'gulp-postcss';
+import cssnext from 'postcss-cssnext';
+import cssnano from 'cssnano';
 
 /**
  * prefix css
  * generate source maps
  */
 export function scss() {
+  const processors = [
+    cssnext,
+    cssnano(),
+  ];
+
   return sass([
     'source/scss/app.scss',
     'source/iconic/scss/iconic-glyphs.scss',
     'source/scss/bootstrap-flex.scss',
     'source/scss/ie.scss',
-  ], { sourcemap: true, style: 'compact' })
-    .pipe(prefix('last 2 version'))
+  ])
+    .pipe(sourcemaps.init())
+    .pipe(postcss(processors))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('public/assets/stylesheets'));
 }

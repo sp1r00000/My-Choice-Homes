e186ad2f32e2f8f7bf17d290e504dd759f40fbbf
d-rivers <d.rivers.wtp@gmail.com>
2016-04-02 13:06:08 +0100

    ripple touch mobile fix

--- source/javascript/common/touch.js
+++ source/javascript/common/touch.js
@@ -9,23 +9,27 @@ const touchElement = function touch() {
 
   helpers.forEach(elements, (index, item) => {
     item.addEventListener('click', event => {
-      if (item.classList.contains('mch-touch')) {
-        const touchX = item.offsetLeft;
-        const touchY = item.offsetTop;
-        const x = event.pageX - touchX;
-        const y = event.pageY - touchY;
-        const width = item.clientWidth;
-        const height = item.clientHeight;
+      event.preventDefault();
 
-        const span = document.createElement('span');
-        span.classList.add('mch-touch-effect');
-        span.style.width = `${width}px`;
-        span.style.height = `${height}px`;
-        span.style.left = `${x - (width / 2)}px`;
-        span.style.top = `${y - (height / 2)}px`;
+      const touchX = item.offsetLeft;
+      const touchY = item.offsetTop;
+      const x = event.pageX - touchX;
+      const y = event.pageY - touchY;
+      const width = item.clientWidth;
+      const height = item.clientHeight;
 
-        item.appendChild(span);
-      }
+      const span = document.createElement('span');
+      span.classList.add('mch-touch-effect');
+      span.style.width = `${width}px`;
+      span.style.height = `${height}px`;
+      span.style.left = `${x - (width / 2)}px`;
+      span.style.top = `${y - (height / 2)}px`;
+
+      item.appendChild(span);
+
+      setTimeout(() => {
+        window.location.pathname = item.pathname;
+      });
     });
   });
 };

7302b706d53d1eebc336df9d5a70220d808b9b8b
d-rivers <d.rivers.wtp@gmail.com>
2016-04-02 22:36:29 +0100

    grayscale toggle

--- source/javascript/common/mch-figure.js
+++ source/javascript/common/mch-figure.js
@@ -1,12 +1,29 @@
 import helpers from '../helpers';
 
 /**
+ * insert a span styled with
+ * inset shadow
+ */
+export function insertShadow() {
+  const elements = document.getElementsByClassName('mch-figure');
+
+  helpers.forEach(elements, (index, item) => {
+    const span = document.createElement('span');
+    span.classList.add('img-inset-shadow');
+    item.appendChild(span);
+  });
+}
+
+export default { insertShadow };
+
+
+/**
  * set the dimensions of elements
  * with class mch-set-dimensions
  * resize event
  */
 export function setDimensions() {
-  const elements = document.getElementsByClassName('mch-set-dimensions');
+  const elements = document.getElementsByClassName('mch-figure');
 
   helpers.forEach(elements, (index, item) => {
     const element = item;
@@ -23,4 +40,4 @@ export function setDimensions() {
   };
 }
 
-export default { setDimensions };
+export default { insertShadow, setDimensions };
--- source/javascript/pages/services.js
+++ source/javascript/pages/services.js
@@ -1,5 +1,34 @@
-import sd from '../common/set-dimensions';
-import is from '../common/insert-shadow';
+import helpers from '../helpers';
+import mchFigure from '../common/mch-figure';
 
-sd.setDimensions();
-is.insertShadow();
+mchFigure.setDimensions();
+mchFigure.insertShadow();
+
+/**
+ * toggle grayscale filter
+ * when element in full view
+ */
+const filterTransition = function filterTransition() {
+  const elements = document.getElementsByClassName('mch-figure');
+
+  function scroll() {
+    helpers.forEach(elements, (index, element) => {
+      const img = element.getElementsByTagName('img')[0];
+      const top = element.offsetTop;
+      const height = element.offsetHeight;
+
+      if (top >= window.pageYOffset
+        && (top + height) <= (window.pageYOffset + window.innerHeight)) {
+        img.classList.remove('grayscale-on');
+        img.classList.add('grayscale-off');
+      } else {
+        img.classList.remove('grayscale-off');
+        img.classList.add('grayscale-on');
+      }
+    });
+  }
+
+  document.onscroll = scroll;
+};
+
+filterTransition();

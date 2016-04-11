ab9fad4276b5e26bd183b403d1b355b2a870b0ec
d-rivers <d.rivers.wtp@gmail.com>
2016-04-11 13:46:15 +0100

    activities block styles

---
+++ source/javascript/views/activities.js
@@ -0,0 +1,12 @@
+import insertContainer from '../common/insert-container';
+
+const container = function container() {
+  insertContainer([
+    'mch-block-1',
+    'mch-block-2',
+    'mch-block-3',
+    'mch-block-4',
+  ]);
+};
+
+container();

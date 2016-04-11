224fd96b9fa7fddcb2de22c1d34f552f16973af0
d-rivers <d.rivers.wtp@gmail.com>
2016-04-11 13:35:09 +0100

    my-choice-school block styles

---
+++ source/javascript/views/my-choice-school.js
@@ -0,0 +1,15 @@
+import insertContainer from '../common/insert-container';
+
+const container = function container() {
+  insertContainer([
+    'mch-block-1',
+    'mch-block-2',
+    'mch-block-3',
+    'mch-block-4',
+    'mch-block-5',
+    'mch-block-6',
+    'mch-block-7',
+  ]);
+};
+
+container();

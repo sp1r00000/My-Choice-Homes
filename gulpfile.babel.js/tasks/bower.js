c19cb08e6c08337d2ab206862862eca977b9f881
System37 <System37@system37.local>
2016-05-05 00:16:44 +0100

    use gsap from cdn

--- gulpfile.babel.js/tasks/bower.js
+++ gulpfile.babel.js/tasks/bower.js
@@ -4,8 +4,6 @@ export function bowerJs() {
   return gulp.src([
     'bower_components/es6-promise-polyfill/promise.min.js',
     'bower_components/requirejs/require.js',
-    'bower_components/gsap/src/minified/TweenMax.min.js',
-    'bower_components/gsap/src/minified/TimeLineMax.min.js',
     'bower_components/babel-polyfill/browser-polyfill.js',
   ])
     .pipe(gulp.dest('public/assets/lib'));
